5e6b575c66d8a6a97805664e94f7146d1d3977d6
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2020-06-02 11:06:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const routes = require('./routes/index')
 const mongoose = require('mongoose');
 
-const port = 5000
+const port = process.env.PORT || 5000;
 const app = express();
 var http = require('http')
 var socketIO = require('socket.io');
